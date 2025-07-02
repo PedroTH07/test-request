c58a35b062c4229b63c7e7ed6b9567bf1894113b
Pedro Silva <pedro.mape7@gmail.com>
2025-07-02 01:37:04 -0300

    feat: update route /

--- src/main/java/com/api/test_request/HttpRequests/HomeController.java
+++ src/main/java/com/api/test_request/HttpRequests/HomeController.java
@@ -1,4 +1,13 @@
 package com.api.test_request.HttpRequests;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping
+    public String homeResponse() {
+        return "Try sending GET ✅, POST ✍, PUT ♻ and DELETE ❌ to path /api to see how this API responds!";
+    }
 }
